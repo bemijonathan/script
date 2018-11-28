17ab66bf57e421e1ff15f3bd7c5832f65493f4fe
bemijonathan <34762800+bemijonathan@users.noreply.github.com>
2018-11-28 15:34:39 +0100

    Add files via upload

---
+++ measurement2.js
@@ -0,0 +1,74 @@
+<li>
+  <span>shoulder :</span>{{measurement?.shoulder}}
+</li>
+<li>
+  <span>back length:</span>{{measurement?.back_length}}
+</li>
+<li>
+  <span>R. Up. Bust:</span>{{measurement?.r_up_bust}}
+</li>
+<li>
+  <span>Bust:</span>{{measurement?.bust}}
+</li>
+<li>
+  <span>R. Un. Bust:</span>{{measurement?.r_un_bust}}
+</li>
+<li>
+  <span>True Waist:</span>{{measurement?.true_waist}}
+</li>
+<li>
+  <span>Blouse Hip:</span>{{measurement?.blouse_hip}}
+</li>
+<li>
+  <span>Full Hips:</span>{{measurement?.full_hips}}
+</li>
+<li>
+  <span>Thigh:</span>{{measurement?.thigh}}
+</li>
+<li>
+  <span>Calf:</span>{{measurement?.calf}}
+</li>
+<li>
+  <span>Sleeve Length:</span>{{measurement?.sleeve_length}}
+</li>
+<li>
+  <span>Sleeve Width:</span>{{measurement?.sleeve_width}}
+</li>
+<li>
+  <span>Neck Dip:</span>{{measurement?.neck_dip}}
+</li>
+<li>
+  <span>Bust Point:</span>{{measurement?.bust_point}}
+</li>
+<ii>
+    <span>und. Bust Point:</span>{{measurement?.und_bust_point}}
+</ii>
+<li>
+  <span>Half Length:</span>{{measurement?.half_length}}
+</li>
+<li>
+  <span>Blouse Length:</span>{{measurement?.blouse_length}}
+</li>
+<li>
+  <span>Hip Length:</span>{{measurement?.hip_length}}
+</li>
+<li>
+  <span>Knee Length:</span>{{measurement?.knee_length}}
+</li>
+<li>
+  <span>3/4 Length:</span>{{measurement?.three_four_length}}
+</li>
+<li>
+  <span>Tea Length:</span>{{measurement?.tea_length}}
+</li>
+<li>
+  <span>Full Length:</span>{{measurement?.full_length}}
+</li>
+<li>
+  <span>Skirt Length:</span>{{measurement?.skirt_length}}
+</li>
+<li>
+  <span>Inseam:</span>{{measurement?.inseam}}
+</li>
+
+
